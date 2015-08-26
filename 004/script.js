b7c40a996bc081f23f6c39395c11f5c05404ea06
Jamie Shepherd <hello@jamie.sh>
2015-08-26 22:30:47 +0100

    Took one assignment out, made it literally 10x faster

--- 004/script.js
+++ 004/script.js
@@ -2,13 +2,11 @@
 var answer = 0;
 
 // Start the program
-var result = 0,
-    palindrome = '';
+var palindrome = '';
 
 for(i = 999; i >= 100; i--) {
     for(j = 999; j>= 100; j--) {
-        result = i * j;
-        palindrome = String(result).split('').reverse().join('');
+        palindrome = String(i*j).split('').reverse().join('');
         if(result == palindrome) {
             if(result > answer) {
                 answer = result;
